9d6e7bf51859b7ac90ed542380a7e95ad77657be
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-10-17 22:20:25 +0500

    list_helper dummy testing

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,12 +2,6 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blogs) =>
-{
-    let sum = 0
-    blogs.forEach(blog => sum+=blog.likes)
-    return sum
-}
 
-module.exports = {dummy,totalLikes}
+module.exports = {dummy}
 
