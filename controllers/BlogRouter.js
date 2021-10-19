876598c0ff48f96b890c9809ed7a8b6f114fbbed
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-10-19 23:13:21 +0500

    delete request with async await

--- controllers/BlogRouter.js
+++ controllers/BlogRouter.js
@@ -34,8 +34,20 @@ BlogRouter.get('/', async (request, response) => {
       response.status(201).json(blog)
     }
 
-   
+  })
 
+  BlogRouter.delete('/:id', async (request, response) => {
+
+    const found = await Blog.findByIdAndDelete(request.params.id)
+    if(found)
+    {
+      response.status(204).send()
+    }
+    else
+    {
+      response.status(400).json({error: "blog not found"})
+    }
+    
   })
 
 module.exports = BlogRouter
