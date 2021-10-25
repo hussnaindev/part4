c3458745832a0322cc076067ed427f868865cf1d
Hussnain Raza ⚡ <hussnain444all@gmail.com>
2021-10-25 23:38:20 +0500

    delete request with auth token update

--- controllers/BlogRouter.js
+++ controllers/BlogRouter.js
@@ -7,97 +7,103 @@ const logger = require('../utils/logger')
 const middleware = require('../utils/middleware')
 const User = require('../models/user.js')
 const jwt = require('jsonwebtoken')
+const blog = require('../models/blog.js')
 
 blogRouter.use(express.json())
 blogRouter.use(cors())
 
 
-blogRouter.get('/', async (request, response) => {
+blogRouter.get('/', async(request, response) => {
     const blogs = await Blog.find({}).populate('user')
-    if(blogs)
-    {
-      response.json(blogs)
+    if (blogs) {
+        response.json(blogs)
+    } else {
+        response.status(404).end()
     }
-    else
-    {
-      response.status(404).end()
-    }
-    
-  })
 
-  blogRouter.post('/', async (request, response) => {
+})
+
+blogRouter.post('/', async(request, response) => {
     logger.info(request.body)
 
-    if(request.body.url==='' || request.body.title==='')
-    {
-      response.status(400).end()
-    }
-    else
-    {
-         const decodedToken = jwt.verify(request.token, process.env.SECRET)
-         logger.info(decodedToken)
-         if (!request.token || !decodedToken.id) 
-         {
+    if (request.body.url === '' || request.body.title === '') {
+        response.status(400).end()
+    } else {
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        logger.info(decodedToken)
+        if (!request.token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
-         }
-          
-          const user = await User.findById(decodedToken.id)
-          logger.info(user)
+        }
 
-          const blog = new Blog({
-          url: request.body.url,
-          title: request.body.title,
-          author: request.body.author,
-          likes: request.body.likes,
-          user: user.id
-          })
+        const user = await User.findById(decodedToken.id)
+        logger.info(user)
 
-  
+        const blog = new Blog({
+            url: request.body.url,
+            title: request.body.title,
+            author: request.body.author,
+            likes: request.body.likes,
+            user: user.id
+        })
+
+
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog.id)
         await user.save()
         response.status(201).json(savedBlog)
     }
-     
 
-  })
 
-  blogRouter.delete('/:id', async (request, response) => {
+})
 
-    const found = await Blog.findByIdAndDelete(request.params.id)
-    if(found)
-    {
-      response.status(204).send()
+blogRouter.delete('/:id', async(request, response) => {
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    logger.info(decodedToken)
+    if (!request.token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
     }
-    else
-    {
-      response.status(400).json({error: "blog not found"})
-    }
-    
-  })
 
-  blogRouter.put('/:id', async (request, response) => {
+    const user = await User.findById(decodedToken.id)
+    logger.info(user)
+    const blog = await Blog.findById(request.params.id)
+    logger.info(blog)
+    logger.info(blog.user.toString())
+    logger.info(user.id)
+
+    if (blog.user.toString() === user.id) {
+        const found = await Blog.findByIdAndDelete(request.params.id)
+        if (found) {
+            response.status(204).send()
+        } else {
+            response.status(400).json({ error: "blog not found" })
+        }
+    } else {
+        response.status(404).send({ error: "user not allowed for this operation" })
+    }
+
+
+})
+
+blogRouter.put('/:id', async(request, response) => {
 
     const updateBlog = {
-       title: request.body.title,
-       author: request.body.author,
-       url: request.body.url,
-       likes: request.body.likes
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
     }
 
-    const foundAndUpdated = await Blog.findByIdAndUpdate(request.params.id, updateBlog )
-    if(foundAndUpdated)
-    {
-      response.json(foundAndUpdated)
+    const foundAndUpdated = await Blog.findByIdAndUpdate(request.params.id, updateBlog)
+    if (foundAndUpdated) {
+        response.json(foundAndUpdated)
+    } else {
+        response.status(400).json({ error: "blog not found" })
     }
-    else
-    {
-      response.status(400).json({error: "blog not found"})
-    }
-    
-  })
 
-  blogRouter.use(middleware.unknownEndpoint)
-  blogRouter.use(middleware.errorHandler)
+})
+
+blogRouter.use(middleware.unknownEndpoint)
+blogRouter.use(middleware.errorHandler)
 
 module.exports = blogRouter
--- models/blog.js
+++ models/blog.js
@@ -6,24 +6,22 @@ const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl).then(logger.info("mongoDB connected"))
 
 const blogSchema = new mongoose.Schema({
-    title: {type:String, default: ''},
-    author: {type:String},
-    url: {type:String, default: ''},
-    likes: {type: Number, default: 0},
-    user: [
-      {
+    title: { type: String, default: '' },
+    author: { type: String },
+    url: { type: String, default: '' },
+    likes: { type: Number, default: 0 },
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-      }
-    ]
-  })
-
-  blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
     }
-  })
+})
 
-  module.exports = mongoose.model('Blog',blogSchema)
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
